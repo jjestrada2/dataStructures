4797f3051d7b87ffe8b6eed7b1181f1ad67fbbf7
Juan Jose Estrada <juan5801331@gmail.com>
2022-05-24 10:29:13 -0700

    create my own array methods

--- arrays.js
+++ arrays.js
@@ -13,6 +13,42 @@
         this.data[this.length]=value
         return this.data
     }
+
+    pop(){
+      const lastItem = this.data[this.length-1]
+      delete this.data[this.length-1]
+      this.length--;
+     return lastItem
+    }
+
+    myDelete(index){
+      const item=this.data[index]
+      this.shiftIndex(index)
+      return item
+    }
+    shiftIndex(index){
+      for(let i=index;i<this.length-1;i++){
+        this.data[i]=this.data[i+1]
+      }
+      delete this.data[index]
+      this.length--;
+    }
+    shift(){
+      item=this.data[0]
+      this.shiftIndex(this.data[0])
+      this.length--;
+      return item
+
+    }
+    unshift(value){
+      this.length++;
+      for(let i=this.length;i>0;i--){
+        this.data[i]=this.data[i-1]
+      }
+      this.length++;
+      this.data[0]=value
+
+    }
   }
 
   const myArray=new MyArray();
