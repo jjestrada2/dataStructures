87e61aaae3a39c7bcbaa0a1a1f39041719b7b892
Juan Jose Estrada <juan5801331@gmail.com>
2022-05-25 12:48:28 -0700

    add my first get all keys method
    git add .:

--- hastable/hastable.js
+++ hastable/hastable.js
@@ -2,12 +2,12 @@ class HashTable{
     constructor(size){
         this.data=new Array(size)
     }
-    hashMethod(key){
-        let hash=0;
-        for(let i= 0;i<key.length;i++){
-            hash=(hash+key.charCode(i)*i)%this.data.length;
+    hashMethod(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+          hash = (hash + key.charCodeAt(i) * i) % this.data.length;
         }
-        return hash
+        return hash;
     }
 
     set(key,value){
@@ -19,6 +19,48 @@ class HashTable{
         return this.data;
 
     }
+    get(key){
+        const adress=this.hashMethod(key)
+        let currentBucket=this.data[adress]
+        if(currentBucket){
+            for(let i=0;i<currentBucket.length;i++){
+                if(currentBucket[i][0]===key){
+                    return currentBucket[i][1]
+                }
+            }
+        }
+
+        return undefined
+    }
+
+    remove(key){
+        const address=this.hashMethod(key)
+        let currentBucket=this.data[address]
+        if(currentBucket){
+            for(let i=0;i<currentBucket.length;i++){
+                if(currentBucket[i][0]===key){
+                    const deleteValue=this.data[address][i]
+                    delete this.data[address][i]
+                    return deleteValue
+                }
+            }
+        }
+        return undefined
+    }
+
+    getAllkeys(){
+        const key=[]
+        for(let i=0;i<this.data.length;i++){
+            if(this.data[i]){
+            for(let j=0;j<this.data[i].length;j++){
+                key.push(this.data[i][j][0])
+                    }
+                }
+            }
+            return key
+    }
+    
+
 }
 
 const myHashTable=new HashTable(50);
